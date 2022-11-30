1729b6925b46020149626e08d9d25fec9a53a9ea
Oliver Oliverio <oliveroliverio@gmail.com>
2022-11-30 10:19:18 -0800

    init

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ import datetime as dt
 import pandas as pd
 
 # asdf = dt.datetime.today(
-# asdf)
+
+stocks = ['AMZN', 'MSFT', 'INTC', 'GOOG']
+
+# around 1 year
+start = dt.datetime.today()-dt.timedelta(360)
+end = dt.datetime.today()
+cl_price = pd.DataFrame() # empty dataframe to be filled
