5c717e0890d6afb249fcbf6e575ea11cd063c3ea
Oliver Oliverio <oliveroliverio@gmail.com>
2022-12-02 15:28:03 -0800

    asdf

--- 23_vis1.py
+++ 23_vis1.py
@@ -16,4 +16,10 @@ cl_price.dropna(axis=0,how='any',inplace=True)
 # important stuff
 daily_rtn = cl_price.pct_change() # new DF showing pct chnge instead of raw price values
 
-cl_price.plot()
+cl_price.plot(subplots=True, layout=(2,2), title="Stock Price Evolution")
+
+daily_rtn.plot(subplots=True, layout=(2,2), title="Daily Rtn")
+
+(1 + daily_rtn).cumprod().plot()
+
+# these plots tell you, if you invested $1 in these stocks in 2012, you would have this much today.  (y axis in dollar amounts)
---
+++ 24_vis2.py
@@ -0,0 +1,14 @@
+import datetime as dt
+import yfinance as yf
+import pandas as pd
+import matplotlib
+
+tickers = ['AMZN', 'MSFT', 'META', 'GOOG']
+start = dt.datetime.today() - dt.timedelta(3650) # 10 years
+end = dt.datetime.today()
+cl_price = pd.DataFrame()
+
+for t in tickers:
+    cl_price[t] = yf.download(t, start, end)['Adj Close']
+
+cl_price.dropna(axis=0,how='any',inplace=True)
