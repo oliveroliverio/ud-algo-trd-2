3146352139839a2f881c70aa23ee35015e5ea857
Oliver Oliverio <oliveroliverio@gmail.com>
2022-12-02 16:03:30 -0800

    asdf

--- 24_vis2.py
+++ 24_vis2.py
@@ -1,7 +1,7 @@
 import datetime as dt
 import yfinance as yf
 import pandas as pd
-import matplotlib
+import matplotlib.pyplot as plt
 
 tickers = ['AMZN', 'MSFT', 'META', 'GOOG']
 start = dt.datetime.today() - dt.timedelta(3650) # 10 years
@@ -12,3 +12,11 @@ for t in tickers:
     cl_price[t] = yf.download(t, start, end)['Adj Close']
 
 cl_price.dropna(axis=0,how='any',inplace=True)
+
+daily_rtn = cl_price.pct_change()
+
+fig, ax = plt.subplots()
+plt.style.available
+plt.style.use('ggplot')
+ax.set(title='Mean Daily Return of Stocks', xlabel='Stocks', ylabel='Mean Return')
+plt.bar(x=daily_rtn.columns, height=daily_rtn.mean())
---
+++ matplotlib_blog.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+
+df = pd.read_excel("https://github.com/chris1610/pbpython/blob/master/data/sample-salesv3.xlsx?raw=true")
